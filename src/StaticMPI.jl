d83089c4bcee638939a5cc4d159ee14877fc0d79
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-07-18 11:55:39 -0400

    Add initial draft of first few functions

--- src/StaticMPI.jl
+++ src/StaticMPI.jl
@@ -1,5 +1,30 @@
 module StaticMPI
 
-# Write your package code here.
+    using StaticTools
+
+    export MPI_Init, MPI_Finalize
+    export MPI_COMM_WORLD, mpi_comm_world
+    export MPI_Comm_size, MPI_Comm_rank
+
+    @inline function MPI_Init(argc::Int=0, argv::Ptr{Ptr{UInt8}}=Ptr{Ptr{UInt8}}(C_NULL))
+        c,v = Ref(argc), Ref(argv)
+        @symbolcall MPI_Init(⅋(c)::Ptr{Int}, ⅋(v)::Ptr{Ptr{Ptr{UInt8}}})::Int32
+        return c[], v[]
+    end
+    @inline MPI_Finalize() = @symbolcall MPI_Finalize()::Int32
+
+    mpi_comm_world() = @externptr ompi_mpi_comm_world::Ptr{UInt8}
+    const MPI_COMM_WORLD = mpi_comm_world
+
+    @inline function MPI_Comm_size(comm::Ptr{Ptr{UInt8}})
+        comm_size = Ref(0)
+        @symbolcall MPI_Comm_size(comm::Ptr{Ptr{UInt8}}, ⅋(comm_size)::Ptr{Int64})::Int32
+        return world_size[]
+    end
+    @inline function MPI_Comm_rank(comm::Ptr{Ptr{UInt8}})
+        comm_rank = Ref(0)
+        @symbolcall MPI_Comm_rank(comm::Ptr{Ptr{UInt8}}, ⅋(comm_rank)::Ptr{Int64})::Int32
+        return comm_rank[]
+    end
 
 end
