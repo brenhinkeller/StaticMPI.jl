799825837b13debf9e923f7eb5ec4136881a1caf
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-07-18 21:13:06 -0400

    Try using OpenMPI_jll for testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using StaticMPI
 using Test
 
+using StaticTools, StaticCompiler
+using OpenMPI_jll
 @testset "StaticMPI.jl" begin
-    # Write your tests here.
+    if OpenMPI_jll.is_available()
+        function mpihello(argc, argv)
+            MPI_Init(argc, argv)
+
+            comm = mpi_comm_world()
+            world_size, world_rank = MPI_Comm_size(comm), MPI_Comm_rank(comm)
+
+            printf((c"Hello from ", world_rank, c" of ", world_size, c" processors!\n"))
+            MPI_Finalize()
+        end
+
+        compile_executable(mpihello, (Int, Ptr{Ptr{UInt8}}), "./";
+            cflags=`-lmpi -L$(OpenMPI_jll.LIBPATH[])`
+        )
+
+        run(`$(OpenMPI_jll.PATH[])/mpiexec --oversubscribe -mp 4 ./mpihello`)
+    end
 end
