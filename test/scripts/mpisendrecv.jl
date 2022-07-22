e5e8315b6032599bdec137ba282872adbc220dbd
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-07-22 14:34:26 -0400

    Update mpisendrecv attempt

--- test/scripts/mpisendrecv.jl
+++ test/scripts/mpisendrecv.jl
@@ -3,22 +3,29 @@ libpath = joinpath(first(splitdir(MPICH_jll.PATH[])), "lib")
 
 function mpisendrecv(argc, argv)
  	status = MPI_Init(argc, argv)
-	(status == MPI_SUCCESS) || error("MPI failed to initialize")
+	if status != MPI_SUCCESS
+		printf(c"ERROR: MPI failed to initialize")::Int32
+	end
 
     comm = MPI_COMM_WORLD
-    world_size, world_rank = MPI_Comm_size(comm), MPI_Comm_rank(comm)
+    world_size = MPI_Comm_size(comm)
+	world_rank = MPI_Comm_rank(comm)
 	nworkers = world_size-1
 
-    if world_rank == 0
-		buffer = mfill(0, nworkers)
+    if world_rank == zero(Int32)
+		buffer = mfill(0.0, nworkers)
 		requests = mfill(MPI_REQUEST_NULL, nworkers)
 		i = 0
 		while i < nworkers
 			i += 1
 			MPI_Irecv(buffer[i:i], i, 0, MPI_COMM_WORLD, requests[i:i])
 		end
-		MPI_Waitall(requests)
+		statuses = MallocArray{StaticMPI.Mpich.MPI_Status,1}(undef, nworkers)
+		index = MPI_Waitall(requests, statuses)
 		printf((c"Rank 0 recieved", buffer, c"\n"))
+		free(statuses)
+		free(requests)
+		free(buffer)
     else
 		rng = BoxMuller(world_rank % Int64)
 		x = randn(rng)
@@ -26,6 +33,7 @@ function mpisendrecv(argc, argv)
 		printf((c"rank ", world_rank, c", generated, ", x, c"\n"))
     end
     MPI_Finalize()
+	return 0
 end
 
 ## ---
